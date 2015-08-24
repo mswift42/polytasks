3203eaec30decf079bc0b037dbbd3cf77fd8ce25
mswift42 <martin.haesler@gmail.com>
2015-08-24 14:21:36 +0100

    add tasks structs.

--- days.go
+++ days.go
@@ -4,8 +4,14 @@ type TaskNote struct {
 	NoteContent string
 }
 
+type TaskCategory struct {
+	Category string
+}
+
 type Task struct {
-	Id      int64    `json:"id" datastore:"-"`
-	Summary string   `json:"summary"`
-	Content TaskNote `json:"content"`
+	Id       int64        `json:"id" datastore:"-"`
+	Summary  string       `json:"summary"`
+	Content  TaskNote     `json:"content"`
+	Done     bool         `json:"done"`
+	Category TaskCategory `json:"category"`
 }
