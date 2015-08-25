8f169b131766d0938ebd1df42cbedd12513c5f3f
mswift42 <martin.haesler@gmail.com>
2015-08-25 17:27:21 +0100

    Add task decode method.

--- days.go
+++ days.go
@@ -1,6 +1,9 @@
 package days
 
 import (
+	"encoding/json"
+	"io"
+
 	"appengine"
 	"appengine/datastore"
 )
@@ -14,7 +17,7 @@ type TaskCategory struct {
 }
 
 type Task struct {
-	Id       int64        `json:"id" datastore:"-"`
+	ID       int64        `json:"id" datastore:"-"`
 	Summary  string       `json:"summary"`
 	Content  TaskNote     `json:"content"`
 	Done     bool         `json:"done"`
@@ -27,4 +30,27 @@ func keyForID(c appengine.Context, id int64) *datastore.Key {
 
 func tasklistkey(c appengine.Context) *datastore.Key {
 	return datastore.NewKey(c, "Task", "default_tasklist", 0, nil)
+}
+
+func (t *Task) key(c appengine.Context) *datastore.Key {
+	if t.ID == 0 {
+		return datastore.NewIncompleteKey(c, "Task", tasklistkey(c))
+	}
+	return datastore.NewKey(c, "Task", "", t.ID, tasklistkey(c))
+}
+
+func (t *Task) save(c appengine.Context) (*Task, error) {
+	k, err := datastore.Put(c, t.key(c), t)
+	if err != nil {
+		return nil, err
+	}
+	t.ID = k.IntID()
+	return t, nil
+}
+
+func decodeTask(r io.ReadCloser) (*Task, error) {
+	defer r.Close()
+	var task Task
+	err := json.NewDecoder(r).Decode(&task)
+	return &task, err
 }
