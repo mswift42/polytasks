387d7a0de31009047ecfa58d9bdd6225ebd47c9f
mswift42 <martin.haesler@gmail.com>
2015-08-27 19:14:54 +0100

    Test decodeTask method.

--- days_test.go
+++ days_test.go
@@ -1,21 +1,20 @@
 package days
 
 import (
+	"io/ioutil"
+	"strings"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 
 	"appengine/aetest"
 )
 
 func TestTaskStruct(t *testing.T) {
-	tn1 := TaskNote{NoteContent: "NoteContent1"}
-	cats := []TaskCategory{
-		TaskCategory{Category: "work"},
-		TaskCategory{Category: "travel"},
-	}
 	t1 := Task{ID: 1, Summary: "task1",
-		Content:  tn1,
+		Content:  []string{"TaskContent1"},
 		Done:     false,
-		Category: cats,
+		Category: []string{"work", "travel"},
 	}
 	if t1.Summary != "task1" {
 		t.Error("expected <task1> got: ", t1.Summary)
@@ -35,4 +34,16 @@ func TestKeyForID(t *testing.T) {
 	if key.IntID() != 0 {
 		t.Error("expected <0>, got: ", key.IntID())
 	}
+}
+
+func TestDecodeTask(t *testing.T) {
+	assert := assert.New(t)
+	var testjson1 = `{"summary" : "task1",
+                "content" : ["taskcontent1"],
+    "done": false}`
+	t1, err := decodeTask(ioutil.NopCloser(strings.NewReader(testjson1)))
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t1.Summary, "task1")
 }
